7f20b15338b702db005ba28263d7bfad6f34930d
ajtroxell <atomtroxell@gmail.com>
2025-02-15 12:48:21 -0600

    Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./src/**/*.{njk,md}'],
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/typography')
+  ],
+  theme: {
+    fontFamily: {
+      'annie': '"Annie Use Your Telescope", Helvetica, Arial, sans-serif',
+      'jakarta': '"Plus Jakarta Sans", Helvetica, Arial, sans-serif',
+    },
+    colors: {
+      'brand-primary': '#dd4343',
+    },
+  },
+  daisyui: {
+    themes: ["retro"],
+  }
+};
